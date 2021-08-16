8bbd3fa124d3467e984e83190420f16e036b5b20
asimpson123 <88037095+asimpson123@users.noreply.github.com>
2021-08-16 12:25:45 +0100

    Add in forecast api and function JS

--- src/app.js
+++ src/app.js
@@ -18,7 +18,8 @@ function formatDate(timestamp) {
   return `${day}: ${hours}:${minutes}`;
 }
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
   let days = ["Thur", "Fri", "Sat", "Sun", "Mon", "Tues"];
@@ -46,6 +47,14 @@ function displayForecast() {
   forecastElement.innerHTML = forecastHTML;
 }
 
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "d918d4788f1c70d2f45815d886132c21";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
@@ -69,6 +78,7 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 
   celsiusTemp = response.data.main.temp;
+  getForecast(response.data.coord);
 }
 
 function search(city) {
@@ -101,8 +111,6 @@ function showCelsiusTemp(event) {
 }
 
 let celsiusTemp = null;
-
-displayForecast();
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
